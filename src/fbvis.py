49bdfc0c5e5e20db35d4e2b4fde9abee11ec618e
Muchen He <mansurhe@outlook.com>
2019-03-03 17:27:47 -0800

    Individual menu selection working

--- src/fbvis.py
+++ src/fbvis.py
@@ -72,9 +72,84 @@ class FBVis:
             printDebug('File structure valid')
 
         # Generate menu from using content within
+        menu_map = {}
+        unknown_index = 0
         for item in os.listdir(messages_path[1]):
-            print(item)
+            # If there is item
+            # msg_file_rel_path = os.path.join(item, fbvis_config.MESSAGE_FILENAME)
+            # msg_file_path = os.path.join(self.rootdir, msg_file_rel_path)
 
+            msg_file_path = os.sep.join([messages_path[1], item, fbvis_config.MESSAGE_FILENAME])
+
+            # If there is something wrong
+            if not os.path.isfile(msg_file_path):
+                printDebug('Warning: messages folder ({}) doesnt have a message json file'.format(msg_file_path))
+                continue
+            
+            # Check that the file only has two participants
+            with open(msg_file_path, encoding='utf-8') as msg_file:
+                msg_data = json.load(msg_file)
+
+                participants = msg_data['participants']
+                if len(participants) > 2:
+                    printDebug('Warning: {} has more than two participants.'.format(msg_file_path))
+                    continue
+
+                # Add other person as a menu item
+                for participant in participants:
+                    name = participant['name']
+
+                    if name != fbvis_config.NAME:
+
+                        # Check name edge cases
+                        if name == 'Facebook User':
+                            name = 'Facebook User ' + str(unknown_index)
+                            unknown_index += 1
+                        elif name in fbvis_config.IGNORED_NAMES:
+                            continue
+                        
+                        # Process what to do with name
+                        if name[0].upper() in menu_map:
+                            menu_map[name[0].upper()][name] = msg_file_path
+                        else:
+                            menu_map[name[0].upper()] = {}
+
+        # Print a sub map that uses alphabets
+        continue_loop = True
+        menu_alphabet_list = [key for key in menu_map]
+        while continue_loop:
+            index = 0
+            for alphabet in menu_alphabet_list:
+                print('[{}]\t{}'.format(index, alphabet))
+                index += 1
+            print('[X]\tBack')
+
+            # Get user input
+            user_input = getMenuInput(menu_alphabet_list)
+
+            if user_input == -1:
+                continue_loop = False
+            else:
+
+                menu_names_list = [name for name in menu_map[menu_alphabet_list[user_input]]]
+
+                # Generate secondary menu
+                continue_inner_loop = True
+                while continue_inner_loop:
+                    inner_index = 0
+                    for name in menu_names_list:
+                        print('[{}]\t{}'.format(inner_index, name))
+                        inner_index += 1
+                    print('[X]\tBack')
+
+                    inner_user_input = getMenuInput(menu_names_list)
+
+                    if inner_user_input == -1:
+                        continue_inner_loop = False
+                    else:
+                        selected_file = menu_map[menu_alphabet_list[user_input]][menu_names_list[inner_user_input]]
+                        printDebug(selected_file + ' selected')
+                        
 
 # Run the program
 if __name__ == '__main__':
--- src/fbvis_config.py
+++ src/fbvis_config.py
@@ -1,8 +1,13 @@
 # Configuration
 
+NAME = 'Muchen He'
+
 # Relative directories
 MESSAGES_INBOX_DIR = ['messages', 'inbox']
+
+MESSAGE_FILENAME = 'message.json'
 
 # User input character
 USER_INPUT_CHAR = '> '
 
+IGNORED_NAMES = ['Messenger']
